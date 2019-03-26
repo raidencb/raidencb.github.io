2bd1679ef25afaee1f46dc3e7c7e846bdcaa88c6
raidencb <reydenn@callboxinc.com>
2019-03-26 11:54:37 +0800

    added some js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-const root = document.createElement("div");
-root.innerHTML = `<p>Hello Webpack.</p>`;
-document.body.appendChild(root);
+import { groupBy } from "lodash-es"
+import people from "./people"
+
+const managerGroups = groupBy(people, "manager")
+
+const root = document.createElement("div")
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
+document.body.appendChild(root)
